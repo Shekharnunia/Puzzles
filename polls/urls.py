8099ef2fd80166c470c4f635e184196ceb124199
shekhar <shekharnunia@gmail.com>
2018-12-28 23:32:58 +0530

    make a form and view to create polls

--- polls/urls.py
+++ polls/urls.py
@@ -5,16 +5,17 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='poll'),
+    path('create', views.create, name='create'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
 
-app_name = 'polls'
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='poll'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-]
+#app_name = 'polls'
+#urlpatterns = [
+#    path('', views.IndexView.as_view(), name='poll'),
+#    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+#    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+#    path('<int:question_id>/vote/', views.vote, name='vote'),
+#]
