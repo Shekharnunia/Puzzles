55af42e956d1fe86a6001bebe9e8d616af1505e5
shekhar nunia <shekharnunia@gmail.com>
2019-02-04 19:36:11 +0530

    form styling change for account verify email template, message box added in polls list, article admin changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,13 @@
-from django import forms
 from django.contrib import admin
 from .models import Article, ArticleComment
 
 
 class ArticleAdmin(admin.ModelAdmin):
-	list_display = ('title', 'user', 'status')
-	list_filter = ('user', 'status', 'timestamp')
-	class Meta:
-		model = Article
+    list_display = ('title', 'user', 'status')
+    list_filter = ('user', 'status', 'timestamp')
+
+    class Meta:
+        model = Article
 
 
 admin.site.register(Article, ArticleAdmin)
