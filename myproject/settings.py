abffba61c9fdada5202993c5df63533290a37b71
shekhar nunia <shekharnunia@gmail.com>
2019-02-13 19:07:24 +0530

    last commit reverted

--- myproject/settings.py
+++ myproject/settings.py
@@ -156,25 +156,25 @@ AUTH_USER_MODEL = 'users.User'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'puzzles',
-#         'USER': 'root',
-#         'PASSWORD': 'shekhar@6',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'sql_mode': 'traditional'
-#         }
-#     }
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'puzzles',
+        'USER': 'root',
+        'PASSWORD': 'shekhar@6',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional'
+        }
+    }
+}
 
 
 # Password validation
