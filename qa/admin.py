21edbaf3f4f459d4b2766c4449d8d4feee9257cd
shekhar nunia <shekharnunia@gmail.com>
2019-02-26 21:24:26 +0530

    css and js links change to cdn and requirement.txt updated

--- qa/admin.py
+++ qa/admin.py
@@ -1,22 +1,24 @@
 from django.contrib import admin
 
-from qa.models import Question, Answer
+from qa.models import Question, Answer, Vote
 
 
 class QuestionAdmin(admin.ModelAdmin):
-	list_display = ['user', 'title', 'status', 'timestamp']
+    list_display = ['user', 'title', 'status', 'timestamp']
 
-	class Meta:
-	    model = Question
+    class Meta:
+        model = Question
 
 
 class AnswerAdmin(admin.ModelAdmin):
-	list_display = ['question', 'user', 'timestamp']
+    list_display = ['question', 'user', 'timestamp']
 
-	class Meta:
-	    model = Answer
+    class Meta:
+        model = Answer
+
 
 admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(Answer, AnswerAdmin)
 
+admin.site.register(Vote)
